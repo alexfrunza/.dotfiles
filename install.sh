b612d56c19c6032c0c8766e930abcc8c0e336e59
Alexandru Frunza <frunza.alexandru.2004@gmail.com>
2022-07-14 21:14:24 +0300

    Add help flag/option and install entire nvim directory

--- install.sh
+++ install.sh
@@ -4,10 +4,13 @@
 # Usage:
 # Make sure this file has execute permission, otherwise:
 # chmod +x install.sh
-# Moving the dotfiles:
+# Installing the dotfiles:
 # ./install.sh
 # Deleting the dotfiles:
 # ./install.sh -r
+# Help menu:
+# ./install.sh -h
+# ./install.sh --help
 # #########################################
 
 # Delete dotfiles utility
@@ -16,7 +19,7 @@ if [ "$1" = "-r" ] ; then
 	if [ "$response" = "y" ] ; then 
 		rm "/home/$USER/.bash_aliases"
 		rm "/home/$USER/.tmux.conf"
-		rm -r "/home/$USER/.config/nvim/init.vim"
+		rm -r "/home/$USER/.config/nvim"
 		echo "Dotfiles deleted successfully!"
 	elif [ "$response" = "n" ] ; then
 		echo "Exiting the program."
@@ -24,6 +27,22 @@ if [ "$1" = "-r" ] ; then
 		echo "Invalid option!"
 	fi
 	exit 0
+elif [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+    echo -e "Usage: 
+    Make sure this file has execute permission, otherwise:
+        chmod +x install.sh
+
+    Installing the dotfiles:
+        ./install.sh
+
+    Deleting the dotfiles:
+        ./install.sh -r
+
+    Help menu (Displays this text):
+        ./install.sh -h
+        ./install.sh --help
+"
+    exit 0
 fi
 
 # Tmux configuration
@@ -31,13 +50,14 @@ cp tmux/.tmux.conf "/home/$USER/.tmux.conf"
 
 # Neovim configuration
 if [ ! -d "/home/$USER/.config/nvim" ] ; then 
-	echo -e "\033[0;33mW: You may need to install vim-plug see: https://github.com/junegunn/vim-plug#neovim\033[0m"
+	echo -e "\033[0;33mW: You may need to install vim-plug see: \
+https://github.com/junegunn/vim-plug#neovim\033[0m"
 	echo "Configuration directory for Neovim doesn't exist... Creating one..."
 	mkdir "/home/$USER/.config/nvim"
 fi
-cp nvim/init.vim "/home/$USER/.config/nvim/init.vim"
+cp -r nvim "/home/$USER/.config"
 
 # Bash aliases
 cp bash/.bash_aliases "/home/$USER/.bash_aliases"
 
-echo "Dotfiles copied succesfully!"
+echo "Dotfiles installed succesfully!"
