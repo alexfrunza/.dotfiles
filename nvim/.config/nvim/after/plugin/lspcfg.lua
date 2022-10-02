3fa72fd86244219882e9c76de4f325116f845543
Alexandru Frunza <frunza.alexandru.2004@gmail.com>
2022-10-02 11:02:16 +0300

    Add lsp for c languages

--- nvim/.config/nvim/after/plugin/lspcfg.lua
+++ nvim/.config/nvim/after/plugin/lspcfg.lua
@@ -1,20 +1,27 @@
+local set_keymaps = function()
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
+    vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer=0})
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer=0})
+    vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, {buffer=0})
+    vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, {buffer=0})
+    -- List of diagnostics in telescope
+    vim.keymap.set("n", "<leader>dl", require'telescope.builtin'.diagnostics, {buffer=0})
+    -- Show details of an error
+    vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, {buffer=0})
+    -- Format code
+    vim.keymap.set("n", "<leader>=", vim.lsp.buf.format, {buffer=0})
+    -- Rename a variable
+    vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {buffer=0})
+end
+
+
 require'lspconfig'.gopls.setup{
     single_file_support=true,
-    on_attach = function()
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
-        vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer=0})
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer=0})
-        vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, {buffer=0})
-        vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, {buffer=0})
-        -- List of diagnostics in telescope
-        vim.keymap.set("n", "<leader>dl", require'telescope.builtin'.diagnostics, {buffer=0})
-        -- Show details of an error
-        vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, {buffer=0})
-        -- Format code
-        vim.keymap.set("n", "<leader>=", vim.lsp.buf.format, {buffer=0})
-        -- Rename a variable
-        vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {buffer=0})
-    end
+    on_attach = set_keymaps,
+}
+
+require'lspconfig'.clangd.setup{
+    on_attach = set_keymaps,
 }
 
