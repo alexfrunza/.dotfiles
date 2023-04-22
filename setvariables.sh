5d75fb969b4370d8a9be49e39f079d4e19a4be90
alexfrunza <frunza.alexandru.2004@gmail.com>
2023-04-22 10:15:33 +0300

    Add config for neofetch

--- setvariables.sh
+++ setvariables.sh
@@ -2,7 +2,7 @@
 
 # You must update the array manually for new configurations
 
-DEFAULT_DIRS=("tmux" "clang-format" "bin" "nvim" "fonts")
+DEFAULT_DIRS=("tmux" "clang-format" "bin" "nvim" "fonts" "neofetch")
 
 # Available configurations for launchers
 launchers=("rofi")
