6a25f921c5dd51da2c8602b93c25b04786ae5671
alexfrunza <frunza.alexandru.2004@gmail.com>
2023-11-25 13:23:52 +0200

    Add lsp for rust

--- nvim/.config/nvim/after/plugin/lspcfg.lua
+++ nvim/.config/nvim/after/plugin/lspcfg.lua
@@ -1,18 +1,23 @@
 local set_keymaps = function()
+  local opts = { buffer = 0 }
+
   -- vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
   -- vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
   -- vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = 0 })
   -- vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = 0 })
-  vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, { buffer = 0 })
-  vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, { buffer = 0 })
+
+  vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, opts)
+  vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, opts)
   -- List of diagnostics in telescope
-  vim.keymap.set("n", "<leader>dl", require 'telescope.builtin'.diagnostics, { buffer = 0 })
+  vim.keymap.set("n", "<leader>dl", require 'telescope.builtin'.diagnostics, opts)
   -- Show details of an error
-  vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { buffer = 0 })
+  vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
   -- Format code
-  vim.keymap.set("n", "<leader>=", vim.lsp.buf.format, { buffer = 0 })
+  vim.keymap.set("n", "<leader>=", vim.lsp.buf.format, opts)
   -- Rename a variable
-  vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { buffer = 0 })
+  vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts)
+
+  vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
 end
 
 
@@ -30,6 +35,11 @@ require 'lspconfig'.eslint.setup {
 }
 
 require 'lspconfig'.pylsp.setup {
+  on_attach = set_keymaps,
+}
+
+require 'lspconfig'.rust_analyzer.setup {
+  -- You need to install this: https://rust-analyzer.github.io/manual.html#rust-analyzer-language-server-binary
   on_attach = set_keymaps,
 }
 
