6b124924589b94cb8d81f084fff28978439daf23
alexfrunza <frunza.alexandru.2004@gmail.com>
2024-02-29 23:27:59 +0200

    Fix prettier and lsp for js

--- nvim/.config/nvim/after/plugin/lspcfg.lua
+++ nvim/.config/nvim/after/plugin/lspcfg.lua
@@ -21,6 +21,29 @@ local set_keymaps = function()
   vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
 end
 
+local set_keymaps_js = function()
+  local opts = { buffer = 0 }
+
+  -- vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
+  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = 0 })
+  -- vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = 0 })
+  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = 0 })
+
+  vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, opts)
+  vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, opts)
+  -- List of diagnostics in telescope
+  vim.keymap.set("n", "<leader>dl", require 'telescope.builtin'.diagnostics, opts)
+  -- Show details of an error
+  vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
+  -- Format code
+  vim.keymap.set("n", "<leader>=", '<cmd>!prettier --write %<cr>', opts)
+  -- Rename a variable
+  vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts)
+
+  vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+end
+
 
 require 'lspconfig'.gopls.setup {
   single_file_support = true,
@@ -32,11 +55,15 @@ require 'lspconfig'.clangd.setup {
 }
 
 require 'lspconfig'.eslint.setup {
-  on_attach = set_keymaps,
+  on_attach = set_keymaps_js,
 }
 
 require 'lspconfig'.pylsp.setup {
   on_attach = set_keymaps,
+}
+
+require 'lspconfig'.tsserver.setup {
+  on_attach = set_keymaps_js,
 }
 
 require 'lspconfig'.rust_analyzer.setup {
