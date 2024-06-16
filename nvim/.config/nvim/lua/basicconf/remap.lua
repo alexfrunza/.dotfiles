3c0c1ec1ed2eb74faab7635e786662941788b3ad
Alexandru Frunza <alex@alexfrunza.com>
2024-06-16 13:58:48 +0300

    Add bindings to start/stop lsp

--- nvim/.config/nvim/lua/basicconf/remap.lua
+++ nvim/.config/nvim/lua/basicconf/remap.lua
@@ -7,4 +7,8 @@ vim.keymap.set("n", "<leader>fg", require 'telescope.builtin'.live_grep)
 
 vim.keymap.set("n", "<leader>zm", "<cmd>ZenMode<cr>")
 
+-- Lsp
+vim.keymap.set("n", "<leader>lp", "<cmd>LspStop<cr>")
+vim.keymap.set("n", "<leader>ls", "<cmd>LspStart<cr>")
+
 -- vim.keymap.set("n", "<leader>=", "<cmd>lua require('conform').format()<cr>")
